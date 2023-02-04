26a66ebc109e2c1b5c55cfb67eaf57fc0d6e8be1
devinguevara <112974756+devinguevara@users.noreply.github.com>
2023-02-04 18:50:45 -0500

    Updated README

--- Code Path iOS/Code Path iOS/ViewController.swift
+++ Code Path iOS/Code Path iOS/ViewController.swift
@@ -16,18 +16,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var schoolnametextfield: UITextField!
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
     @IBOutlet weak var numberofPetsLabel: UILabel!
-
+    
+    
+    @IBOutlet weak var pronounsoutlet: UITextField!
+    
+    @IBOutlet weak var coloroutlet: UIButton!
+    
+    @IBAction func coloraction(_ sender: UIButton) {
+        self.view.backgroundColor = UIColor.lightGray
+    }
+    
     @IBAction func actionpetstepper(_ sender: UIStepper) {
         //so it seems numberofPetsLabel.text subsets the text value of that label, and then sender.value gets the number the stepper is currently on
         numberofPetsLabel.text = "\(Int(sender.value))"
     }
+    
+    
+    
     
     @IBAction func actionintroduceself(_ sender: UIButton) {
         let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex) // this means the selected option in the IB outlet yearSegmentedControl.
         
         //introduction statement
         //apparently I have to write the text all within the ident and adjacent to each other or it will not read as text
-        let introduction = "My name is \(firstnametextfield.text!) \(lastnametextfield.text!) and I attend \(schoolnametextfield.text!). I am a \(year!) year and I own \(numberofPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        //I added pronouns since I think that is an important part of any introduction
+        
+        let introduction = " My name is \(firstnametextfield.text!), I use \(pronounsoutlet.text!) pronouns, and I am a \(year!) year  at \(schoolnametextfield.text!). I have \(numberofPetsLabel.text!) pets, and it is \(morePetsSwitch.isOn) that I want more pets."
         
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
         let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# codepathIOS-prework
-Codepath introduce yourself application 
+## NAME_OF_YOUR_APP_HERE
+
+### App Description
+
+This application was made as part of the 102 iOS Development Codepath pre-work assignment. The application asks information about the user and then introduces that person in a display message after the user submits their info.
+
+### App Walk-though
+
+<img src="https://i.imgur.com/AA00S3F.gif" width=200><br> 
+
+### Required Features
+
+- [x] 1. App displays an image of a school's logo
+- [x] 2. App has three textfields for first, last, and school names
+- [x] 3. App has a segmented control that changes student year
+- [x] 4. Number of pet matches label is increased/decreased by stepper
+- [x] 5. Switch makes a statement about wanting more pets or not(true/false) 
+- [x] 6. Introduce yourself button shows alert box with an introduciton and dismiss button
+
+### Optional Features
+
+- [x] 1. User can tap a button to change the color of the background view
+- [x] 3. User can select on additional buttons that provide more info about the user. Example: more textfields, a different alert box, etc.
+- [ ] 4. Any stylistic changes that are not default options (Comment this here)
